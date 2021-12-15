60d78f7a69873a04b6f257cfaf74a1e4e315e1e8
andrey <stackkk@yandex.ru>
2021-12-15 13:27:35 +0300

    third

--- first_table/views.py
+++ first_table/views.py
@@ -4,33 +4,38 @@ from django.core.paginator import Paginator
 
 
 def index(request):
+    qs = Table.objects.all()
+
     if request.method == 'POST':
         print(request.POST)
-        if request.POST['condition'] == 'more' and request.POST['column'] == 'number':    #количество больше
-            qs = Table.objects.filter(number__gt = int(request.POST['value']))
-        elif request.POST['condition'] == 'more' and request.POST['column'] == 'distance':  #расстояние больше
-            qs = Table.objects.filter(distance__gt = int(request.POST['value']))
-        elif request.POST['condition'] == 'less' and request.POST['column'] == 'number':   #количество меньше
-            qs = Table.objects.filter(number__lt = int(request.POST['value']))
-        elif request.POST['condition'] == 'less' and request.POST['column'] == 'distance':#расстояние меньше
-            qs = Table.objects.filter(distance__lt = int(request.POST['value']))
-        elif request.POST['condition'] == 'more':           #больше- работает на названиях (по алфавиту)
-            qs = Table.objects.order_by(request.POST['column'])
-        elif request.POST['condition'] == 'less':           #меньше - работает на названиях  (по алфавиту)
-            qs = Table.objects.order_by(f"-{request.POST['column']}")
-        elif request.POST['condition'] == 'equals' and request.POST['column'] == 'title':           #точное совпадение названия
-            qs = Table.objects.raw(f"select * from first_table_table where {request.POST['column']} like '{request.POST['value']}'")
-        elif request.POST['condition'] == 'contains' and request.POST['column'] == 'title':            #совпадение названий по входящим словам или буквам
-            qs = Table.objects.raw(f"select * from first_table_table where title like '%%{request.POST['value']}%%'")
-        elif request.POST['column'] == 'distance' and request.POST['condition'] == 'equals':        #точное расстояние
-            qs = Table.objects.filter(distance=int(request.POST['value']))
-        elif request.POST['column'] == 'number' and request.POST['condition'] == 'equals':          #точное количество
-            qs = Table.objects.filter(number=int(request.POST['value']))
-        elif request.POST['column'] == 'number' and request.POST['condition'] == 'contains':    #количество "содержит" (больше либо равно)
-            qs = Table.objects.filter(number__gte=int(request.POST['value']))
-        elif request.POST['column'] == 'distance' and request.POST['condition'] == 'contains':  #расстояние "содержит" (больше либо равно)
-            qs = Table.objects.filter(distance__gte=int(request.POST['value']))
-        paginator = Paginator(qs, 4)
+        if request.POST['condition'] == 'more' and not request.POST['value'] and request.POST['column'] == 'title':        #название - больше (по алфавиту)
+            result = qs.order_by(request.POST['column'])
+        elif request.POST['condition'] == 'less' and not request.POST['value'] and request.POST['column'] == 'title':       #название - меньше (по алфавиту)
+            result = qs.order_by(f"-{request.POST['column']}")
+        elif request.POST['condition'] == 'more' and request.POST['column'] == 'number' and request.POST['value']:    #количество больше
+            result = qs.filter(number__gt = int(request.POST['value']))
+        elif request.POST['condition'] == 'more' and request.POST['column'] == 'distance' and request.POST['value']:  #расстояние больше
+            result = qs.filter(distance__gt = int(request.POST['value']))
+        elif request.POST['condition'] == 'less' and request.POST['column'] == 'number' and request.POST['value']:   #количество меньше
+            result = qs.filter(number__lt = int(request.POST['value']))
+        elif request.POST['condition'] == 'less' and request.POST['column'] == 'distance' and request.POST['value']: #расстояние меньше
+            result = qs.filter(distance__lt = int(request.POST['value']))
+        elif request.POST['condition'] == 'equals' and request.POST['column'] == 'title':       #название - равно
+            result = qs.filter(title__iexact = request.POST['value'])
+        elif request.POST['condition'] == 'contains' and request.POST['column'] == 'title':       #название - содержит
+            result = qs.filter(title__icontains=request.POST['value'])
+        elif request.POST['column'] == 'distance' and request.POST['condition'] == 'equals' and request.POST['value']:    #расстояние - равно
+            result = qs.filter(distance=int(request.POST['value']))
+        elif request.POST['column'] == 'number' and request.POST['condition'] == 'equals' and request.POST['value']:     #количество - равно
+            result = qs.filter(number=int(request.POST['value']))
+        elif request.POST['column'] == 'number' and request.POST['condition'] == 'contains' and request.POST['value']:    #количество "содержит" (больше либо равно)
+            result = qs.filter(number__gte=int(request.POST['value']))
+        elif request.POST['column'] == 'distance' and request.POST['condition'] == 'contains' and request.POST['value']:  #расстояние "содержит" (больше либо равно)
+            result = qs.filter(distance__gte=int(request.POST['value']))
+        else:
+            result = qs.all()
+
+        paginator = Paginator(result, 4)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         context = {'page_obj': page_obj}
--- welbex/settings.py
+++ welbex/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'first_table',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
