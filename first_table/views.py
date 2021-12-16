b5464134806ac2210483c41c5e9604cb6293927d
andrey <stackkk@yandex.ru>
2021-12-16 14:40:55 +0300

    finish

--- first_table/views.py
+++ first_table/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from .models import Table
 from django.core.paginator import Paginator
 
+qs = Table.objects.all()
+
 
 def index(request):
-    qs = Table.objects.all()
     paginator = Paginator(qs, 4)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -13,42 +14,36 @@ def index(request):
 
 
 def sorts(request):
-    qs = Table.objects.all()
+    result = qs
     if request.method == 'POST':
-        if request.POST['condition'] == 'more' and not request.POST['value'] and request.POST[
-            'column'] == 'title':  # название - больше (по алфавиту)
-            result = qs.order_by(request.POST['column'])
-        elif request.POST['condition'] == 'less' and not request.POST['value'] and request.POST[
-            'column'] == 'title':  # название - меньше (по алфавиту)
-            result = qs.order_by(f"-{request.POST['column']}")
-        elif request.POST['condition'] == 'more' and request.POST['column'] == 'number' and request.POST[
-            'value']:  # количество больше
-            result = qs.filter(number__gt=int(request.POST['value']))
-        elif request.POST['condition'] == 'more' and request.POST['column'] == 'distance' and request.POST[
-            'value']:  # расстояние больше
-            result = qs.filter(distance__gt=int(request.POST['value']))
-        elif request.POST['condition'] == 'less' and request.POST['column'] == 'number' and request.POST[
-            'value']:  # количество меньше
-            result = qs.filter(number__lt=int(request.POST['value']))
-        elif request.POST['condition'] == 'less' and request.POST['column'] == 'distance' and request.POST[
-            'value']:  # расстояние меньше
-            result = qs.filter(distance__lt=int(request.POST['value']))
-        elif request.POST['condition'] == 'equals' and request.POST['column'] == 'title':  # название - равно
-            result = qs.filter(title__iexact=request.POST['value'])
-        elif request.POST['condition'] == 'contains' and request.POST['column'] == 'title':  # название - содержит
-            result = qs.filter(title__icontains=request.POST['value'])
-        elif request.POST['column'] == 'distance' and request.POST['condition'] == 'equals' and request.POST[
-            'value']:  # расстояние - равно
-            result = qs.filter(distance=int(request.POST['value']))
-        elif request.POST['column'] == 'number' and request.POST['condition'] == 'equals' and request.POST[
-            'value']:  # количество - равно
-            result = qs.filter(number=int(request.POST['value']))
-        elif request.POST['column'] == 'number' and request.POST['condition'] == 'contains' and request.POST[
-            'value']:  # количество "содержит" (больше либо равно)
-            result = qs.filter(number__gte=int(request.POST['value']))
-        elif request.POST['column'] == 'distance' and request.POST['condition'] == 'contains' and request.POST[
-            'value']:  # расстояние "содержит" (больше либо равно)
-            result = qs.filter(distance__gte=int(request.POST['value']))
+        if request.POST['condition'] == 'more':                                 # значение больше
+            if not request.POST['value']:
+                result = qs.order_by(request.POST['column'])                    # название - больше (по алфавиту)
+            elif request.POST['value'] and request.POST['column'] == 'number':
+                result = qs.filter(number__gt=int(request.POST['value']))       # количество больше
+            elif request.POST['value'] and request.POST['column'] == 'distance':
+                result = qs.filter(distance__gt=int(request.POST['value']))      # расстояние больше
+        elif request.POST['condition'] == 'less':                                # значение меньше
+            if not request.POST['value']:
+                result = qs.order_by(f"-{request.POST['column']}")               # название - меньше (по алфавиту)
+            elif request.POST['value'] and request.POST['column'] == 'number':
+                result = qs.filter(number__lt=int(request.POST['value']))       # количество меньше
+            elif request.POST['value'] and request.POST['column'] == 'distance':
+                result = qs.filter(distance__lt=int(request.POST['value']))     # расстояние меньше
+        elif request.POST['condition'] == 'contains':                            # значение содержит
+            if request.POST['column'] == 'title':
+                result = qs.filter(title__icontains=request.POST['value'])      # название содержит
+            elif request.POST['column'] == 'number' and request.POST['value']:
+                result = qs.filter(number__gte=int(request.POST['value']))       # количество "содержит" (больше либо равно)
+            elif request.POST['column'] == 'distance' and request.POST['value']:
+                result = qs.filter(distance__gte=int(request.POST['value']))     # расстояние "содержит" (больше либо равно)
+        elif request.POST['condition'] == 'equals':                              # значение равно
+            if request.POST['column'] == 'title':
+                result = qs.filter(title__iexact=request.POST['value'])          # название равно
+            elif request.POST['column'] == 'number' and request.POST['value']:
+                result = qs.filter(number=int(request.POST['value']))            # количество равно
+            elif request.POST['column'] == 'distance' and request.POST['value']:
+                result = qs.filter(distance=int(request.POST['value']))           # расстояние равно
         else:
             result = qs.all()
 
@@ -56,5 +51,5 @@ def sorts(request):
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         context = {'page_obj': page_obj}
-
         return render(request, 'first_table/sort.html', context)
+
