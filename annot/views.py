76608fd59c3ae064d15e506c3d78845e0d8ba2e5
andrey <stackkk@yandex.ru>
2021-12-30 22:07:05 +0300

    finish

--- annot/views.py
+++ annot/views.py
@@ -8,4 +8,5 @@ def main(request, pk):
     qs = Task.objects.filter(project_id=pk).annotate(total_working_time = Count('taskontime__time'))
     print(list(qs))
     print(vars(qs[0]))
+    print(qs[0].total_working_time)
     return HttpResponse(qs[0])
